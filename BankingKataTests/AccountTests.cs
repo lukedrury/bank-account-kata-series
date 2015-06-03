0c7737cc6c383261c4fbb4ef7e48835bee0794be
Luke Drury <luke.drury@red-gate.com>
2015-06-03 16:08:43 +0100

    green: last transaction in multiples is that transaction

--- BankingKataTests/AccountTests.cs
+++ BankingKataTests/AccountTests.cs
@@ -18,5 +18,19 @@ namespace BankingKataTests
             var expectedTransaction = new Transaction(new Money(0), new DateTime(0));
             Assert.That(lastTransaction, Is.EqualTo(expectedTransaction));
         }
+
+        [Test]
+        public void TheLastTransactionInAMultipleTransactionAccountIsThatTransaction()
+        {
+            var account = new Account();
+
+            account.Deposit(new Money(0), new DateTime(0));
+            account.Deposit(new Money(1), new DateTime(1));
+            account.Deposit(new Money(2), new DateTime(2));
+            var lastTransaction = account.LastTransaction();
+
+            var expectedTransaction = new Transaction(new Money(2), new DateTime(2));
+            Assert.That(lastTransaction, Is.EqualTo(expectedTransaction));
+        }
     }
 }
