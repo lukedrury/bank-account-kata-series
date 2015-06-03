4a30640b0824b593a1fd63be107d1b3a9a85e8bd
Luke Drury <luke.drury@red-gate.com>
2015-06-03 16:23:38 +0100

    Make tests not use zero for money

--- BankingKataTests/AccountTests.cs
+++ BankingKataTests/AccountTests.cs
@@ -12,10 +12,10 @@ namespace BankingKataTests
         {
             var account = new Account();
 
-            account.Deposit(new Money(0), new DateTime(0));
+            account.Deposit(new Money(1), new DateTime(0));
             var lastTransaction = account.LastTransaction();
 
-            var expectedTransaction = new Transaction(new Money(0), new DateTime(0));
+            var expectedTransaction = new Transaction(new Money(1), new DateTime(0));
             Assert.That(lastTransaction, Is.EqualTo(expectedTransaction));
         }
 
@@ -24,12 +24,12 @@ namespace BankingKataTests
         {
             var account = new Account();
 
-            account.Deposit(new Money(0), new DateTime(0));
-            account.Deposit(new Money(1), new DateTime(1));
-            account.Deposit(new Money(2), new DateTime(2));
+            account.Deposit(new Money(1), new DateTime(0));
+            account.Deposit(new Money(2), new DateTime(1));
+            account.Deposit(new Money(3), new DateTime(2));
             var lastTransaction = account.LastTransaction();
 
-            var expectedTransaction = new Transaction(new Money(2), new DateTime(2));
+            var expectedTransaction = new Transaction(new Money(3), new DateTime(2));
             Assert.That(lastTransaction, Is.EqualTo(expectedTransaction));
         }
 
