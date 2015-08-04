eff576bb3694f3686bed790d4d4a56cc6744c8ed
Luke Drury <luke.drury@red-gate.com>
2015-08-04 10:52:33 +0100

    Red: withdrawal should have ATM in print

--- BankingKataTests/PrintBalanceTest.cs
+++ BankingKataTests/PrintBalanceTest.cs
@@ -85,7 +85,7 @@ namespace BankingKataTests
             account.PrintLastTransaction(printer);
 
             var output = stringWriter.GetStringBuilder();
-            var expected = "Last transaction: 13 Jul 2015 (£123.00)";
+            var expected = "Last transaction: ATM 13 Jul 2015 (£123.00)";
             Assert.That(output.ToString(), Is.EqualTo(expected));
         }
     }
